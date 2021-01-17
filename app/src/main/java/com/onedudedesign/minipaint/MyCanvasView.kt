b7c96db5caa6ed7958009d9b8dcc885c022fe427
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-17 09:28:26 -0800

    Added a freme inset from the edge in the current paint color

--- app/src/main/java/com/onedudedesign/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/onedudedesign/minipaint/MyCanvasView.kt
@@ -1,10 +1,7 @@
 package com.onedudedesign.minipaint
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
@@ -24,6 +21,7 @@ class MyCanvasView(context: Context) : View(context) {
     private lateinit var extraBitmap: Bitmap
 
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+    private lateinit var frame: Rect
 
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
@@ -52,12 +50,17 @@ class MyCanvasView(context: Context) : View(context) {
         extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
         extraCanvas.drawColor(backgroundColor)
+        // Calculate a rectangular frame around the picture.
+        val inset = 40
+        frame = Rect(inset, inset, w - inset, h - inset)
 
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawBitmap(extraBitmap,0f,0f,null)
+        // Draw a frame around the canvas.
+        canvas?.drawRect(frame, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
